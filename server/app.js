3e6330c92fb01b478fce5fe44df523b325da5da8
nhilicious <nhi.phamt2002@gmail.com>
2023-05-02 16:48:00 +0700

    add router for dish and category API

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ import dashboardRouter from './routes/dashboard.router.js';
 import uploadRouter from './routes/uploadImage.router.js';
 import exportDataRouter from './routes/exportData.router.js';
 import studentRouter from './routes/student.router.js';
+import categoryRouter from "./routes/category.router.js";
+import dishRouter from "./routes/dish.router.js";
 
 dotenv.config();
 const app = express();
@@ -26,6 +28,9 @@ app.use('/dashboard', dashboardRouter);
 app.use('/upload', uploadRouter);
 app.use('/export', exportDataRouter);
 app.use('/students', studentRouter);
+
+app.use('/category', categoryRouter);
+app.use("/dish", dishRouter);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
