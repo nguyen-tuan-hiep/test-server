24fe6c6272803b1d7ec8cb24d8df258a0b6c43a2
Phạm Thảo Nhi <nhi.pt205190@sis.hust.edu.vn>
2023-08-18 19:47:02 +0700

    update event api

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -58,7 +58,49 @@ async function updateEvent(req, res) {
     return res.status(500).json({ message: error.message });
   }
 }
+async function updateEventIncludeDish(req, res) {
+  try {
+    const { name, description, beginTime, closeTime, poster, dishId } =
+      req.body;
+    const { id } = req.params;
 
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required' });
+    }
+    // Update event if it exists
+    const event = await pool.query(
+      'UPDATE events SET event_name = $1, description = $2, begin_time = $3, close_time = $4, poster = $5 WHERE event_id = $6 RETURNING *',
+      [name, description, beginTime, closeTime, poster, id],
+    );
+    // Check if event exists
+    if (!event.rows.length) {
+      return res.status(404).json({ message: 'Event not found' });
+    }
+
+    let resData = { events: event.rows[0], dishes: [] };
+    // Update event_dishes table
+
+    // Delete all dishes of event
+    await pool.query('DELETE FROM event_dishes WHERE event_id = $1', [id]);
+
+    const eventId = event.rows[0].event_id;
+    if (dishId) {
+      // Insert new dishes of event
+      const values = dishId.map((id) => `(${eventId}, ${id})`).join(', ');
+      const dishes = await pool.query(
+        `INSERT INTO event_dishes (event_id, dish_id) VALUES ${values} RETURNING *;`,
+      );
+      resData.dishes = dishes.rows;
+    }
+
+    return res
+      .status(200)
+      .json({ message: 'Event was updated!', data: resData });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+}
 async function createEvent(req, res) {
   try {
     const { name, description, beginTime, closeTime, poster } = req.body;
@@ -75,6 +117,38 @@ async function createEvent(req, res) {
       'INSERT INTO events (event_name, description, begin_time, close_time, poster) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [name, description, beginTime, closeTime, poster],
     );
+    return res
+      .status(200)
+      .json({ message: 'Event was created!', data: event.rows[0] });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+}
+async function createEventIncludeDish(req, res) {
+  try {
+    const { name, description, beginTime, closeTime, poster, dishId } =
+      req.body;
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required' });
+    }
+    if (!beginTime) {
+      return res.status(400).json({ message: 'Begin time is required' });
+    }
+    if (!closeTime) {
+      return res.status(400).json({ message: 'End time is required' });
+    }
+    const event = await pool.query(
+      'INSERT INTO events (event_name, description, begin_time, close_time, poster) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [name, description, beginTime, closeTime, poster],
+    );
+    const eventId = event.rows[0].event_id;
+    // dishId is an array
+    if (dishId) {
+      const values = dishId.map((id) => `(${eventId}, ${id})`).join(', ');
+      const query = `INSERT INTO event_dishes (event_id, dish_id) VALUES ${values} RETURNING *;`;
+      const eventDishes = await pool.query(query);
+    }
     return res
       .status(200)
       .json({ message: 'Event was created!', data: event.rows[0] });
@@ -128,6 +202,21 @@ async function searchEventByName(req, res) {
   }
 }
 
+async function getAllDishesOfEvent(req, res) {
+  try {
+    const { id } = req.params;
+    const dishes = await pool.query(
+      'SELECT dish_id FROM event_dishes WHERE event_id = $1',
+      [id],
+    );
+    
+    return res.status(200).json(dishes.rows);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
 export default {
   getAllEvents,
   deleteEvent,
@@ -135,4 +224,7 @@ export default {
   createEvent,
   getEventById,
   searchEventByName,
+  createEventIncludeDish,
+  updateEventIncludeDish,
+  getAllDishesOfEvent,
 };
--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -13,10 +13,19 @@ router.get('/search', eventController.searchEventByName);
 router.delete('/:id', eventController.deleteEvent);
 
 // update
-router.patch('/:id', eventController.updateEvent);
+// router.post('/:id', eventController.updateEvent);
+
+// update include dish (delete event_dish and insert new event_dish)
+router.post('/:id', eventController.updateEventIncludeDish);
 
 // create
 router.post('/create', eventController.createEvent);
+
+//create 
+router.post('/createv2', eventController.createEventIncludeDish);  
+
+// get all dishes of an event
+router.get('/getDishes/:id', eventController.getAllDishesOfEvent);
 
 // get
 router.get('/:id', eventController.getEventById);
